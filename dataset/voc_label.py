f7e9c9ce88aa9e9938f5569c844e5ebe8377e1c5
taiyuan <taiyuan.fang@gmail.com>
2020-05-07 22:33:46 +0800

    modified:   dataset/voc_label.py modified:   person/data.txt

--- dataset/voc_label.py
+++ dataset/voc_label.py
@@ -16,8 +16,7 @@ if len(sys.argv) > 2:
 sets = [('2012', 'train'), ('2012', 'val'),
         ('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
 
-classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow",
-           "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+classes = ["person"]
 
 
 def convert(size, box):
